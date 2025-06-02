5e5a736a0bd1926d37f6f3de8242e3b424b214cb
webxrahat <islammdrahat308@gmail.com>
2025-06-02 17:51:33 +0600

    user data

--- src/components/OrderBoard.jsx
+++ src/components/OrderBoard.jsx
@@ -4,18 +4,65 @@ import OrderSummary from "./OrderSummary";
 import OrderReportsList from "./OrderReportsList";
 import { useState } from "react";
 
+const orderTables = {
+  orderItems: [
+    {
+      id: crypto.randomUUID(),
+      name: "Hamburger",
+      src: "./assets/hamburger.svg",
+      taka: "250",
+    },
+    {
+      id: crypto.randomUUID(),
+      name: "Chicken Nuggets",
+      src: "./assets/chicken.svg",
+      taka: "450",
+    },
+    {
+      id: crypto.randomUUID(),
+      name: "Submarine Sandwich",
+      src: "./assets/submarine.svg",
+      taka: "400",
+    },
+    {
+      id: crypto.randomUUID(),
+      name: "Pizza slices",
+      src: "./assets/pizza.svg",
+      taka: "150",
+    },
+  ],
+};
+
+const customerOrder = {
+  userName: "",
+  totalOrder: [],
+};
+
 export default function OrderBoard() {
-  const [inputValue, setInputValue] = useState({});
+  const [orders, setOrders] = useState(orderTables);
+  const [orderAmounts, setOrderAmounts] = useState("");
+  const [inputText, setInputText] = useState("");
+
+  const [userOrder, setUserOrder] = useState(customerOrder);
+
+  // console.log(orders);
+  console.log(orderAmounts);
+  console.log(inputText);
 
   const handleInputChange = (e) => {
-    const value = e.target.value;
-    setInputValue({ value });
+    const name = e.target.value;
+    setUserOrder(name);
+    // setInputText(e.target.value);
+    // console.log(value);
   };
-  console.log(inputValue);
+
+  const handleFoodMenu = (foodItem) => {
+    // console.log(foodItem);
+    setOrderAmounts(foodItem.taka);
+  };
 
   const handleSubmitForm = (e) => {
     e.preventDefault();
-    setInputValue(inputValue);
   };
 
   return (
@@ -24,8 +71,10 @@ export default function OrderBoard() {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 flex-grow">
         <OrderCreate
-          handleSubmitForm={handleSubmitForm}
+          orders={orders}
           handleInputChange={handleInputChange}
+          handleFoodMenu={handleFoodMenu}
+          handleSubmitForm={handleSubmitForm}
         />
 
         <div className="md:col-span-2 h-[calc(100vh_-_130px)]">
--- src/components/OrderCreate.jsx
+++ src/components/OrderCreate.jsx
@@ -1,31 +1,9 @@
-const foodlist = [
-  {
-    id: crypto.randomUUID(),
-    name: "Hamburger",
-    src: "./assets/hamburger.svg",
-    taka: "250",
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Chicken Nuggets",
-    src: "./assets/chicken.svg",
-    taka: "450",
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Submarine Sandwich",
-    src: "./assets/submarine.svg",
-    taka: "400",
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Pizza slices",
-    src: "./assets/pizza.svg",
-    taka: "150",
-  },
-];
-
-export default function OrderCreate({ handleSubmitForm, handleInputChange }) {
+export default function OrderCreate({
+  handleSubmitForm,
+  handleInputChange,
+  handleFoodMenu,
+  orders,
+}) {
   return (
     <div className="bg-gray-300 rounded-lg p-6 h-[calc(100vh_-_130px)]">
       <h2 className="text-xl font-bold mb-1">CREATE ORDER</h2>
@@ -40,6 +18,7 @@ export default function OrderCreate({ handleSubmitForm, handleInputChange }) {
             Customer Name
           </label>
           <input
+            name="userName"
             onChange={handleInputChange}
             type="text"
             className="w-full bg-gray-700 bg-opacity-50 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-primary transition-all duration-300 text-white"
@@ -49,27 +28,31 @@ export default function OrderCreate({ handleSubmitForm, handleInputChange }) {
         <div className="mb-4">
           <label className="block text-sm font-medium mb-2">Choose Items</label>
           <div className="items-container">
-            {foodlist.map((food) => {
+            {orders?.orderItems?.map((signOrder) => {
               return (
                 <div
-                  key={food.id}
+                  key={signOrder.id}
                   className="bg-gray-700 bg-opacity-30 rounded-md p-3 mb-3 flex justify-between items-center hover:bg-opacity-40 transition-all duration-300"
                 >
                   <div className="flex items-center">
                     <div className="w-12 h-12   flex items-center justify-center mr-3">
                       <img
-                        src={food.src}
-                        alt={food.name}
+                        src={signOrder.src}
+                        alt={signOrder.name}
                         className="w-10 h-10"
                       />
                     </div>
                     <div>
-                      <h3 className="font-medium text-gray-100">{food.name}</h3>
-                      <p className="text-xs text-gray-300">BDT {food.taka}</p>
+                      <h3 className="font-medium text-gray-100">
+                        {signOrder.name}
+                      </h3>
+                      <p className="text-xs text-gray-300">
+                        BDT {signOrder.taka}
+                      </p>
                     </div>
                   </div>
                   <button
-                    onClick={() => handleFoodMenu(food.id)}
+                    onClick={() => handleFoodMenu(signOrder)}
                     className="w-8 h-8 bg-gray-800 hover:bg-primary rounded-full flex items-center justify-center transition-colors duration-300"
                   >
                     <svg
@@ -88,6 +71,49 @@ export default function OrderCreate({ handleSubmitForm, handleInputChange }) {
                 </div>
               );
             })}
+            {/* {orders?.orderItems?.map((order) => {
+              console.log("sing order", order);
+
+              return (
+                <div
+                  key={order.id}
+                  className="bg-gray-700 bg-opacity-30 rounded-md p-3 mb-3 flex justify-between items-center hover:bg-opacity-40 transition-all duration-300"
+                >
+                  <div className="flex items-center">
+                    <div className="w-12 h-12   flex items-center justify-center mr-3">
+                      <img
+                        src={order.src}
+                        alt={order.name}
+                        className="w-10 h-10"
+                      />
+                    </div>
+                    <div>
+                      <h3 className="font-medium text-gray-100">
+                        {order.name}
+                      </h3>
+                      <p className="text-xs text-gray-300">BDT {order.taka}</p>
+                    </div>
+                  </div>
+                  <button
+                    onClick={() => handleFoodMenu(order)}
+                    className="w-8 h-8 bg-gray-800 hover:bg-primary rounded-full flex items-center justify-center transition-colors duration-300"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 text-green-500 cursor-pointer"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              );
+            })} */}
 
             {/* <div className="bg-gray-700 bg-opacity-30 rounded-md p-3 mb-3 flex justify-between items-center hover:bg-opacity-40 transition-all duration-300">
               <div className="flex items-center">
