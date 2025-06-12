5ca591d2b6a465cd92be5a1e3e1212acaeaa58cd
Md Rahat Islam <islammdrahat308@gmail.com>
2025-06-12 07:38:45 +0600

    do not add duplicate

--- src/components/OrderBoard.jsx
+++ src/components/OrderBoard.jsx
@@ -5,51 +5,68 @@ import OrderReportsList from "./OrderReportsList";
 import { useState } from "react";
 
 const customerOrder = {
-  userName: "",
-  totalOrder: [],
+ userName: "",
+ totalOrder: [],
 };
 
 export default function OrderBoard() {
-  const handleSubmitForm = (e) => {
-    e.preventDefault();
-  };
+ const handleSubmitForm = (e) => {
+  e.preventDefault();
+ };
 
-  const [getOrders, setGetOrders] = useState(customerOrder);
-  // const [addOrder, setAddOrder] = useState(true);
+ const [getOrders, setGetOrders] = useState(customerOrder);
+ // const [addOrder, setAddOrder] = useState(true);
 
-  console.log(getOrders);
+ console.log(getOrders);
 
-  const curentOrder = (value) => {
-    setGetOrders((prv) => ({
-      ...prv,
-      userName: value,
-    }));
-  };
+ const curentOrder = (value) => {
+  setGetOrders((prv) => ({
+   ...prv,
+   userName: value,
+  }));
+ };
 
-  const orderItemCreate = (signOrder) => {
-    setGetOrders((prv) => ({
-      ...prv,
-      totalOrder: [...prv.totalOrder, signOrder],
-    }));
-  };
+ //  const orderItemCreate = (singleOrder) => {
+ //   setGetOrders((prv) => ({
+ //    ...prv,
+ //    totalOrder: [...prv.totalOrder, singleOrder],
+ //   }));
+ //   };
 
-  return (
-    <div className="container mx-auto px-4 h-screen flex flex-col">
-      <NavBar />
+ const orderItemCreate = (singleOrder) => {
+  setGetOrders((prev) => {
+   const alreadyExists = prev.totalOrder.some(
+    (order) => order.id === singleOrder.id
+   );
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 flex-grow">
-        <OrderCreate
-          curentOrder={curentOrder}
-          handleSubmitForm={handleSubmitForm}
-          orderItemCreate={orderItemCreate}
-        />
+   if (alreadyExists) {
+    return prev;
+   }
 
-        <div className="md:col-span-2 h-[calc(100vh_-_130px)]">
-          <OrderSummary />
+   return {
+    ...prev,
+    totalOrder: [...prev.totalOrder, singleOrder],
+   };
+  });
+ };
 
-          <OrderReportsList />
-        </div>
-      </div>
+ return (
+  <div className="container mx-auto px-4 h-screen flex flex-col">
+   <NavBar />
+
+   <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 flex-grow">
+    <OrderCreate
+     curentOrder={curentOrder}
+     handleSubmitForm={handleSubmitForm}
+     orderItemCreate={orderItemCreate}
+    />
+
+    <div className="md:col-span-2 h-[calc(100vh_-_130px)]">
+     <OrderSummary />
+
+     <OrderReportsList />
     </div>
-  );
+   </div>
+  </div>
+ );
 }
